cc3aed8fa35dba575bea2162ad11a401e3c9b363
netbug94 <bongohmo@gmail.com>
2024-07-25 11:31:05 -0700

    testing deploy

--- src/home_page/HomePagePreLoader.js
+++
@@ -1,6 +0,0 @@
-
-export default function preloadImages(pictures) {
-    pictures.forEach(picture => {
-        new Image().src = picture.fileName;
-    });
-}
--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import HomePageIndex from './HomePageIndex.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HomePageIndex />
-  </React.StrictMode>,
-)
